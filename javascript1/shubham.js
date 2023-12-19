9b8ab6ae273ccfb9dfe1514078f75771d549329e
Shubham <shubhamchendka94@gmail.com>
2023-12-19 03:32:45 -0800

    shashank

---
+++ javascript1/shubham.js
@@ -0,0 +1,39 @@
+ //var a = "Shubham";
+ //console.log(a);
+
+ //alert(" Shubham ");
+ //console.log(alert);
+
+// console.log("shubham");
+
+ //console.log (age = 24);
+ //console.log (age + 1);
+
+ //console.log ("Shubham",age = 24+1);
+
+//Full_Name = "Surya";
+//Age = 25;
+//Department = "Developer";
+//Salary = Null;
+//Experience = 3; 
+
+//console.log(Salary);
+
+//let a = 100;
+//b = 50;
+//x = a + b;
+
+//console.log(x);
+
+//let x = 100 + 50;
+//console.log(x);
+
+
+/*let a = 100;
+let b = 50;
+let x = a + b;
+console.log(x);*/
+
+let a = 3;
+let x = (100 + 50) * a;
+console.log(x);
